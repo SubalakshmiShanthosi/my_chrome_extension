985fd9f013dac51fa3c28dbf1afd57fd68a6abd4
SubalakshmiShanthosi <subalakshmicv@gmail.com>
2024-02-12 16:50:32 -0600

    Adding current date,time and functionality to update timestamp every 15 seconds

---
+++ popup.js
@@ -0,0 +1,22 @@
+setTimeout(refresh, 1000);
+
+setTimeout(setClock, 1000);
+
+function setClock() {
+    const clock = document.getElementById('clock');
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    clock.innerText = document.title = hours + ":" + minutes
+}
+
+function refresh() {
+    var currentdate = new Date();
+    const options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
+    const time = new Intl.DateTimeFormat("en-US", options).format();
+    document.getElementById("time").innerHTML = time;
+}
